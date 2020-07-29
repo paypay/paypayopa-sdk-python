db6df6a0f9d2162f3316c16db146595c9243192c
Srinivas Gowda <srinivas@solancer.com>
2020-07-29 08:48:42 +0530

    Update production URL

--- paypayopa/constants/url.py
+++ paypayopa/constants/url.py
@@ -1,5 +1,5 @@
 class URL(object):
     SANDBOX_BASE_URL = 'https://stg-api.sandbox.paypay.ne.jp/v2'
-    PRODUCTION_BASE_URL = 'https://api-global.paypay.ne.jp/v2'
+    PRODUCTION_BASE_URL = 'https://api.paypay.ne.jp/v2'
     CODE = "/codes"
     PAYMENT = "/payments"
