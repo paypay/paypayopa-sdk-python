7371c7c7d30a0e007968f2d223b0172a6622af5e
Srinivas Gowda <srinivas@solancer.com>
2020-08-20 02:44:44 +0530

    Update path without duplicate v2 path

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="paypayopa",
-    version="0.3.7",
+    version="0.3.8",
     description="PayPay OPA SDK",
     long_description=long_description,
     long_description_content_type='text/markdown',
