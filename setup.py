3543a67b18d536f3decc86f8bd960d677366921c
Shreyansh Pandey <shreyansh.pandey@paypay-corp.co.jp>
2020-09-09 15:51:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="paypayopa",
-    version="0.4.2",
+    version="0.5.0",
     description="PayPay OPA SDK",
     long_description=long_description,
     long_description_content_type='text/markdown',
