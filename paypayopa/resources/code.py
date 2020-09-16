c0b54a411091514ea67e2c40cfaffd98619e93b3
Srinivas Gowda <srinivas@solancer.com>
2020-09-16 12:03:21 +0530

    FIx url enpoints for code and revert payment resource endpoint

--- paypayopa/resources/code.py
+++ paypayopa/resources/code.py
@@ -41,6 +41,13 @@ class Code(Resource):
                                  " \x1b[0m for orderItem.amount.currency")
         return self.post_url(url, data, **kwargs)
 
+    def get_payment_details(self, id, **kwargs):
+        url = "{}/{}/{}".format(self.base_url, 'payments', id)
+        if id is None:
+            raise ValueError("\x1b[31m MISSING REQUEST PARAMS"
+                             " \x1b[0m for merchantPaymentId")
+        return self.fetch(None, url, **kwargs)
+
     def delete_qr_code(self, id=None, **kwargs):
         if id is None:
             raise ValueError("\x1b[31m MISSING REQUEST PARAMS"
