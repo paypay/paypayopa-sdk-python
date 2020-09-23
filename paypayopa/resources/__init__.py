d8f934d945cb9518ad4c79315718a6fdd659a279
Srinivas Gowda <srinivas@solancer.com>
2020-09-23 11:13:54 +0530

    Add pending payments resource to SDK

--- paypayopa/resources/__init__.py
+++ paypayopa/resources/__init__.py
@@ -2,10 +2,12 @@ from .code import Code
 from .payment import Payment
 from .account import Account
 from .preauth import Preauth
+from .pending import Pending
 
 __all__ = [
         'Code',
         'Payment',
         'Account',
         'Preauth',
+        'Pending',
 ]
