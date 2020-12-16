d4d71aaa116028fe29ff22459f6a613d7e2746f2
ashraf-km <ashraf.km@robosoftin.com>
2020-12-16 09:27:40 +0530

    test cancel payment, capture payment

---
+++ tests/test_capture_payment.py
@@ -0,0 +1,21 @@
+import responses
+import json
+
+from .helpers import mock_file, ClientTestCase
+
+
+class TestCapturePayment(ClientTestCase):
+
+    def setUp(self):
+        super(TestCapturePayment, self).setUp()
+        self.base_url = '{}/payments/capture'.format(
+            self.base_url)
+
+    @responses.activate
+    def testcapture_payment(self):
+        init = mock_file('capture_payment_payload')
+        result = mock_file('capture_payment_response')
+        url = self.base_url
+        responses.add(responses.POST, url, status=200, body=json.dumps(result),
+                      match_querystring=True)
+        self.assertEqual(self.client.Payment.capture_payment(init), result)
