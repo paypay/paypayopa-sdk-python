c3b22f1f93946951edaecf339a9e9871bc203820
ashraf-km <ashraf.km@robosoftin.com>
2020-12-16 14:55:18 +0530

    test create payment

---
+++ tests/test_refund_details.py
@@ -0,0 +1,21 @@
+import responses
+import json
+
+from .helpers import mock_file, ClientTestCase
+
+
+class TestRefundDetails(ClientTestCase):
+
+    def setUp(self):
+        super(TestRefundDetails, self).setUp()
+        self.base_url = '{}/refunds/fake_merchant_refundId'.format(
+            self.base_url)
+
+    @responses.activate
+    def test_refund_details(self):
+        result = mock_file('refund_details')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200, body=json.dumps(result),
+                      match_querystring=True)
+        self.assertEqual(self.client.Payment.refund_details(
+            'fake_merchant_refundId'), result)
---
+++ tests/test_unlink_user.py
@@ -0,0 +1,21 @@
+import responses
+import json
+
+from .helpers import mock_file, ClientTestCase
+
+
+class TestUnlinkUser(ClientTestCase):
+
+    def setUp(self):
+        super(TestUnlinkUser, self).setUp()
+        self.base_url = '{}/user/authorizations/fake_user_id'.format(
+            self.base_url)
+
+    @responses.activate
+    def test_unlink_user(self):
+        result = mock_file('unlink_user')
+        url = '{}/{}'.format(self.base_url, 'fake_user_id')
+        responses.add(responses.DELETE, url, status=200, body=json.dumps(result),
+                      match_querystring=True)
+        self.assertEqual(self.client.User.unlink_user_athorization(
+            'fake_user_id'), result)
