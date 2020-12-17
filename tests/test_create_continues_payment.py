47073ca13bd0cbc3700095fd5e76d7cf88ad320b
ashraf-km <ashraf.km@robosoftin.com>
2020-12-17 06:12:30 +0530

    test continues payment

---
+++ tests/test_create_continues_payment.py
@@ -0,0 +1,20 @@
+import responses
+import json
+
+from .helpers import mock_file, ClientTestCase
+
+
+class TestCreateContinuesPayment(ClientTestCase):
+
+    def setUp(self):
+        super(TestCreateContinuesPayment, self).setUp()
+
+    @responses.activate
+    def test_CreateContinuesPayment(self):
+        init = mock_file('create_continues_payment_payload')
+        result = mock_file('create_continues_payment_response')
+        url = "https://stg-api.sandbox.paypay.ne.jp/v1/subscription/payments"
+        responses.add(responses.POST, url, status=200, body=json.dumps(result),
+                      match_querystring=True)
+        self.assertEqual(
+            self.client.Payment.create_continuous_payment(init), result)
--- tests/test_get_payment_qr_details.py
+++ tests/test_get_payment_qr_details.py
@@ -4,18 +4,18 @@ import json
 from .helpers import mock_file, ClientTestCase
 
 
-class TestGetPaymentDetails(ClientTestCase):
+class TestGetPaymentQRDetails(ClientTestCase):
 
     def setUp(self):
-        super(TestGetPaymentDetails, self).setUp()
-        self.base_url = '{}/payments/fake_merchant_payment_id'.format(
+        super(TestGetPaymentQRDetails, self).setUp()
+        self.base_url = '{}/codes/payments/fake_merchant_payment_id'.format(
             self.base_url)
 
     @responses.activate
-    def test_get_payment_details(self):
+    def test_get_payment_qr_details(self):
         result = mock_file('get_payment_details')
         url = self.base_url
         responses.add(responses.GET, url, status=200, body=json.dumps(result),
                       match_querystring=True)
-        self.assertEqual(self.client.Payment.get_payment_details(
+        self.assertEqual(self.client.Code.get_payment_details(
             'fake_merchant_payment_id'), result)
