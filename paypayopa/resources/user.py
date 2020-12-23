5e401e19b592b6eb8b78e44c41f2aab9256c2674
ashraf-km <ashraf.km@robosoftin.com>
2020-12-23 09:51:33 +0530

    codeclimate issue fix

--- paypayopa/resources/user.py
+++ paypayopa/resources/user.py
@@ -20,7 +20,7 @@ class User(Resource):
 
     def unlink_user_athorization(self, id=None, **kwargs):
         if id is None:
-            raise ValueError("\x1b[31m MISSING REQUESTs PARAMS"
-                             " \x1b[0m for codeId")
+            raise ValueError("\x1b[31m MISSING REQUEST PARAMS"
+                             " \x1b[0m for code_id")
         url = "{}/{}".format(self.base_url, id)
         return self.delete(None, url, api_id=API_NAMES.UNLINK_USER, **kwargs)
