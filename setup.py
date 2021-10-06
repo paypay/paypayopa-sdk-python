3f9890486a5f829df6731c327c173b995fb81a58
Curtis Fenner <t-curtis.fenner@paypay-corp.co.jp>
2021-10-06 12:24:54 -0400

    Replace setup.py with example from BlackDuckCoPilot

--- setup.py
+++ setup.py
@@ -1,43 +1,19 @@
-from setuptools import setup
 
-# from os import path
-# this_directory = path.abspath(path.dirname(__file__))
-# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#     long_description = f.read()
-long_description = "a long description"
+from setuptools import setup, find_packages
+
+package_name = "copilot_example_pip_travis"
+package_version = "1.0.0"
 
 setup(
-    name="paypayopa",
-    version="0.0.0",
-    description="PayPay OPA SDK",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author="Team PayPay",
-    author_email="opensource@paypay-corp.co.jp",
-    license="Apache 2.0",
-    project_urls={
-        'Documentation': 'https://github.com/paypay/paypayopa-sdk-python/README.md',
-        'Source': 'https://github.com/paypay/paypayopa-sdk-python',
-    },
-    url='https://github.com/paypay/paypayopa-sdk-python',
-    install_requires=["requests", "pyjwt"],
-    include_package_data=True,
-    package_dir={'paypayopa': 'paypayopa',
-                 'paypayopa.resources': 'paypayopa/resources',
-                 'paypayopa.constants': 'paypayopa/constants'},
-    packages=['paypayopa', 'paypayopa.resources', 'paypayopa.constants'],
-    keywords='paypay payment gateway japan',
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-
-        # List of supported Python versions
-        # Make sure that this is reflected in .travis.yml as well
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
+    name=package_name,
+    version=package_version,
+    author="Black Duck CoPilot",
+    author_email="copilot@blackducksoftware.com",
+    url="https://github.com/BlackDuckCoPilot/example-pip-travis",
+    install_requires=["requests", "numpy", "Django==1.9.6"],
+    entry_points={
+      'console_scripts': [
+          'example-pip-travis=Main:main'
+      ]
+    }
 )
